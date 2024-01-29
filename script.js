9c268d4671588bd253e238864469c18b2b1b6eec
“Federico <federicomanna01@yahoo.it>
2024-01-29 22:17:35 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//  ESERCIZI:
+//  1  Crea una funzione che controlli due numeri interi. Ritorna true se uno dei due è 50 o se la somma dei due è 50.
+// function numb() {
+//     let x = 25;
+//     let y = 25;
+//     if ((x === 50 || y === 50) || (x + y === 50)) {
+//         return console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+// numb()
+//  2  Crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la posizione come parametri e ritorna la stringa modificata.
+
+// let string = "ciao";
+// let pos = 2;
+// function remove(myStr, myPos) {
+//     let modify = myStr.slice(pos);
+//     console.log(modify)
+// }
+
+// remove(string, pos)
+//  3  Crea una funzione che controlli se due numeri siano compresi tra 40 e 60 o tra 70 e 100. Ritorna
+// 'true' se rispecchiano queste condizioni, altrimenti ritorna 'false'.
+
+// 4. Crea una funzione che accetti un nome di città come parametro e ritorni il nome stesso se inizia con
+// "Los" o "New", altrimenti ritorni false'.
+// 5. Crea una funzione che calcoli e ritorni la somma di tutti gli elementi di un array.
+// L'array deve essere passato come parametro.
+// 6. Crea una funzione che controlli che un array NON contenga i numeri 1 o 3. Se NON li contiene, ritorna
+// "true', altrimenti ritorna 'false'.
+
+
+// ESERCIZI:
+// 7. Crea una funzione per trovare il tipo di un angolo i cui gradi sono passati come parametro.
+// Angolo acuto: meno di 90° → ritorna "acuto"
+// Angolo ottuso: tra i 90° e i 180° gradi → ritorna "ottuso"
+// Angolo retto: 90° → ritorna "retto"
+// Angolo piatto: 180° → ritorna "piatto"
+// 8. Crea una funzione che crei un acronimo a partire da una frase. Es. "Fabbrica Italiana Automobili
+// Torino" deve ritornare "FIAT".
