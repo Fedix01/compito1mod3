bc03698a22ed4cf776347ba4ebf0f14614905fc8
“Federico <federicomanna01@yahoo.it>
2024-01-30 20:40:37 +0100

    exercise done

--- script.js
+++ script.js
@@ -36,17 +36,93 @@
 // numb()
 // 4. Crea una funzione che accetti un nome di città come parametro e ritorni il nome stesso se inizia con
 // "Los" o "New", altrimenti ritorni false'.
+// let city = "Los Angeles"
+
+// function searchCity(città) {
+//     if ((città.includes("Los")) || (città.includes("New"))) {
+//         return console.log(città)
+//     } else {
+//         return console.log(false)
+//     }
+// }
+// searchCity(city);
 // 5. Crea una funzione che calcoli e ritorni la somma di tutti gli elementi di un array.
 // L'array deve essere passato come parametro.
+
+// let arr = [10, 20, 30];
+// function somma(myArr) {
+//     let sum = 0;
+//     for (let i = 0; i < myArr.length; i++) {
+//         sum += myArr[i];
+//     } return console.log(sum)
+// }
+// somma(arr)
 // 6. Crea una funzione che controlli che un array NON contenga i numeri 1 o 3. Se NON li contiene, ritorna
 // "true', altrimenti ritorna 'false'.
 
-
+// let array = [0, 2, 4]
+// function noNumb() {
+//     if ((array.includes(1)) || (array.includes(3))) {
+//         return console.log(false)
+//     } else {
+//         return console.log(true)
+//     }
+// }
+// noNumb()
 // ESERCIZI:
 // 7. Crea una funzione per trovare il tipo di un angolo i cui gradi sono passati come parametro.
 // Angolo acuto: meno di 90° → ritorna "acuto"
 // Angolo ottuso: tra i 90° e i 180° gradi → ritorna "ottuso"
 // Angolo retto: 90° → ritorna "retto"
 // Angolo piatto: 180° → ritorna "piatto"
+// let angle = 100;
+// function findAngle(angle) {
+//     switch (angle) {
+//         case 1:
+//             angle < 90;
+//             console.log("acuto")
+//             break;
+//         case 2:
+//             angle > 90 && angle < 180;
+//             console.log("ottuso");
+//             break;
+//         case 3:
+//             angle === 90;
+//             console.log("retto");
+//             break;
+//         case 4:
+//             angle === 180;
+//             console.log("piatto");
+//             break;
+//     }
+// oppure...
+//     if (angle < 90) {
+//         return console.log("acuto");
+//     } else if (angle > 90 && angle < 180) {
+//         return console.log("ottuso");
+//     } else if (angle === 90) {
+//         return console.log("retto");
+//     } else if (angle === 180) {
+//         return console.log("piatto")
+//     }
+// }
+// findAngle(angle)
 // 8. Crea una funzione che crei un acronimo a partire da una frase. Es. "Fabbrica Italiana Automobili
 // Torino" deve ritornare "FIAT".
+
+// function getFirstLetter(string) {
+//     let words = string.split(" ");
+//     let firstLetters = [];
+//     for (let i = 0; i < words.length; i++) {
+//         let word = words[i];
+//         let firstLetter = word.charAt(0);
+//         firstLetters.push(firstLetter)
+//     }
+//     let result = firstLetters.join("")
+//     return result
+// }
+
+// let stringa = "Hyper Text Markup Language";
+
+// let firstLetter = getFirstLetter(stringa);
+// console.log(firstLetter)
